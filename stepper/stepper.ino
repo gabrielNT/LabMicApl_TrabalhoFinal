86af1aed6a6d6869c1eadc189e604f3e5441e72e
Guilherme Ramos <gmatheus95@outlook.com>
2015-12-02 15:34:02 -0200

    Programa Teste LCD + Motor de passo
    Teste bem-sucedido

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -1,11 +1,18 @@
+#include <rgb_lcd.h>
+#include <Wire.h>
 #include <Stepper.h>
 #define STEPS 100
 
 Stepper stepper(STEPS, 8, 9, 11, 10);
+rgb_lcd lcd;
+
 
 void setup() {
   stepper.setSpeed(100);
-
+  lcd.begin(16,2);
+  lcd.setRGB(0,0,255);
+  lcd.print("Teste do LCD");
+  delay(1000);
 }
 
 void loop() {
